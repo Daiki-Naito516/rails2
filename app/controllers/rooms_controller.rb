db4b11e35c5331d140eda2d861d2f707f3a32460
Daiki Naito <daikinaito@DaikinoMacBook-Air.local>
2023-01-08 13:52:19 +0900

    roomの画像を保存してlistingに表示させる

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,83 @@
+  class RoomsController < ApplicationController
+    protect_from_forgery except: [:upload_photo]
+    before_action :set_room, except: [:index, :new, :create]
+    before_action :authenticate_user!, except: [:show]
+    before_action :is_authorised, only: [:listing, :pricing, :description, :photo_upload, :amenities, :location, :update]
+
+    def index
+       @rooms = current_user.rooms
+    end
+
+    def new
+      @room = current_user.rooms.build
+    end
+
+    def create
+      @room = current_user.rooms.build(room_params)
+      if @room.save
+        redirect_to listing_room_path(@room), notice: "保存しました。"
+      else
+        flash[:alert] = "未入力の項目があります。"
+        render :new
+      end
+    end
+
+    def show
+    end
+
+    def listing
+    end
+
+    def pricing
+    end
+
+    def description
+    end
+
+    def photo_upload
+    end
+
+    def amenities
+    end
+    
+    def location
+    end
+
+    def update
+      new_params = room_params
+      new_params = room_params.merge(active: true) if is_ready_room
+      if @room.update(new_params)
+        flash[:notice] = "保存しました。"
+      else
+        flash[:alert] = "問題が発生しました。"
+      end
+      redirect_back(fallback_location: request.referer)
+    end
+    def upload_photo
+      @room.photos.attach(params[:file])
+      render json: { success: true }
+    end
+    def delete_photo
+      @image = ActiveStorage::Attachment.find(params[:photo_id])
+      @image.purge
+      redirect_to photo_upload_room_path(@room)
+    end
+    private
+      def set_room
+        @room = Room.find(params[:id])
+      end
+      
+      private
+
+    def room_params
+      params.require(:room).permit(:image, :listing_name, :summary, :price, :address, :active)
+    end
+    
+      def is_authorised
+        redirect_to root_path, alert: "権限がありません。" unless current_user.id == @room.user_id
+      end
+      
+      def is_ready_room
+        !@room.active && !@room.price.blank? && !@room.listing_name.blank? && !@room.photos.blank? && !@room.address.blank?
+      end
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/new'
+  get 'rooms/create'
+  get 'rooms/listing'
+  get 'rooms/pricing'
+  get 'rooms/description'
+  get 'rooms/photo_upload'
+  get 'rooms/amenities'
+  get 'rooms/location'
+  get 'rooms/update'
   devise_for :users
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
